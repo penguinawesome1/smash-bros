9e9ea3411a2bbd8bd3d0383835a8b2e8edd5e323
penguinawesome1 <owen.h.colley@gmail.com>
2025-01-23 14:11:08 -0500

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -4,7 +4,7 @@ function collision({ object1, object2 }) {
         object1.position.y <= object2.position.y + object2.height &&
         object1.position.x <= object2.position.x + object2.width &&
         object1.position.x + object1.width >= object2.position.x
-    )
+    );
 }
   
 function platformCollision({ object1, object2 }) {
